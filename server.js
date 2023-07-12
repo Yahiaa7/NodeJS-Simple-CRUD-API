669cd936b2c7bd33b632817910d03e11d4ce995f
Yahiaa7 <yihya7.ya@gmail.com>
2023-07-12 18:26:48 +0300

    make images folder accessible on server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+app.use('/images', express.static('images'));
 
 // connect and sync the DB 
 dbConnectSync();
